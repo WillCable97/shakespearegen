85712d75d4f8034dabdc3bad268cf404406ba59b
WillCable97 <94039566+WillCable97@users.noreply.github.com>
2024-02-05 01:50:43 -0500

    Increased epoch and variable sequence length

--- src/gru_runner.py
+++ src/gru_runner.py
@@ -3,40 +3,42 @@ from models.GRU_model import GRU_model
 import tensorflow as tf
 import os
 
-char_indexing_obj = CharacterIndexing()
-data_object_initial = DataObject(hugging_face_name="tiny_shakespeare"
-                                 ,index_obj=char_indexing_obj
-                                 ,sequence_length=100
-                                 ,batch_size=64
-                                 ,buffer_size=10000)
 
 
-vocab_size = len(data_object_initial.text_to_int())
-gru_model = GRU_model(vocab_size, 256, 1024, 64)
+sequence_lengths = [50,100,150,200]
 
+EPOCHS=20
 
 #Define some things for compilation
 def loss_func(labels, logits):
     return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
+char_indexing_obj = CharacterIndexing()
 
-gru_model.compile(optimizer='adam', loss=loss_func)
-
-
-csv_dir = './src/models/callbacks/gru/csv'
-checkpoint_dir = './src/models/callbacks/gru/checkpoint'
-
-
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
-csv_prefix = os.path.join(csv_dir, "csv_tracker")
-
-checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_prefix,
-    save_weights_only=True)
+for sequence in sequence_lengths:
+    data_object_initial = DataObject(hugging_face_name="tiny_shakespeare"
+                                    ,index_obj=char_indexing_obj
+                                    ,sequence_length=sequence
+                                    ,batch_size=64
+                                    ,buffer_size=10000)
+    
+    vocab_size = len(data_object_initial.text_to_int())
+    gru_model = GRU_model(vocab_size, 256, 1024, 64)
+    gru_model.compile(optimizer='adam', loss=loss_func)
 
 
-csv_callback = tf.keras.callbacks.CSVLogger(csv_prefix)
+    model_base_path = f"./src/models/callbacks/gru/sequence_{sequence}"
+    checkpoint_prefix = os.path.join(model_base_path, "checkpoint", "ckpt_{epoch}")
+    csv_prefix = os.path.join(model_base_path, "csv", "csv_tracker")
 
-EPOCHS=10
-history = gru_model.fit(data_object_initial.final_dataset, epochs=10, callbacks=[checkpoint_callback, csv_callback])
+
+    checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_prefix,
+        save_weights_only=True)
+
+
+    csv_callback = tf.keras.callbacks.CSVLogger(csv_prefix)
+    
+    history = gru_model.fit(data_object_initial.final_dataset, epochs=10, callbacks=[checkpoint_callback, csv_callback])
+
 
